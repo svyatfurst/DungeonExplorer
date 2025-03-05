177a232e4239826d899f6a3a63eb188e134606b7
svyatfurst <gromalex27@gmail.com>
2025-03-05 20:50:47 +0000

    added music, images

--- Game.cs
+++ Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Media;
 
 namespace DungeonExplorer
@@ -9,38 +10,63 @@ namespace DungeonExplorer
         private Player player;
         private Room currentRoom;
         private string roomItem;
+        private string music;
 
-        public Game(string name, string room, string item)
+        public Game(string name, string room, string item, string music)
         {
             // Initialize the game with one room and one player
             player = new Player(name, 50);
             currentRoom = new Room(room);
             roomItem = item;
+            music = music;
+        }
+        public void WriteCentered(string text)
+        {
+            int width = Console.WindowWidth;
+            string[] lines = text.Split('\n');
+
+            foreach (string line in lines)
+            {
+                string trimmedLine = line.TrimEnd();
+                int leftPadding = Math.Max((width - trimmedLine.Length) / 2, 0);
+                Console.WriteLine(new string(' ', leftPadding) + trimmedLine);
+            }
         }
         public void Start()
         {
             // Change the playing logic into true and populate the while loop
             bool playing = true;
-            int choice;
-            Console.WriteLine(
+            this.WriteCentered(
                 $"You woke up in the {currentRoom.GetDescription()}"
             );
             while (playing)
             {
+                SoundPlayer player = new SoundPlayer(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"{music}.wav"));
+                player.Play();
                 // Code your playing logic here
-                Console.Write(
+                Console.WriteLine(
                     "What do you want to do?\n" +
                     "1 - Look around\n" +
                     "2 - View Inventory\n" +
                     "3 - View Stats"
                 );
+                int action = -1;
+                string choice;
                 do
                 {
                     Console.Write("Your input: ");
-                    choice = int.Parse(Console.ReadLine());
+                    choice = Console.ReadLine();
+                    if (int.TryParse(choice, out action))
+                    {
+                        action = int.Parse(choice);
+                    }
+                    else
+                    {
+                        continue;
+                    }
                 } while (
-                !new List<int> { 1, 2, 3 }.Contains(choice)
-            );
+                    !new List<int> { 1, 2, 3 }.Contains(action)
+                );
             }
         }
     }
--- Player.cs
+++ Player.cs
@@ -7,6 +7,8 @@ namespace DungeonExplorer
     {
         public string Name { get ; private set; }
         public int Health { get; private set; }
+        private List<string> inventory = new List<string>();
+
         public Player(string name, int health) 
         {
             Name = name;
